2a940173b0e88217f6e0a056f27e503628da0e53
AlvesDario <dario.alves@fatec.sp.gov.br>
2019-02-12 15:58:01 -0200

    dia 2: functions

---
+++ Exercicios/3_Functions.js
@@ -0,0 +1,19 @@
+/*
+    Exercicio 1)
+        Write a function min that takes two arguments and returns their minimum.
+*/
+const min = (n1, n2) => {
+    return n1<n2?n1:n2;
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+
+/*
+    Exercicio 2)
+        
+*/
+
+const isEven = (n) => {
+
+}
